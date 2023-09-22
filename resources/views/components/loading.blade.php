c55f3c4d183c981cf2ab0f44ab9a5d6b29176979
GevorgyanSam <SamvelGevorgyanPC@gmail.com>
2023-09-22 21:41:30 +0400

    SpyBird New Loading Animation (1.1.11)

--- resources/views/components/loading.blade.php
+++ resources/views/components/loading.blade.php
@@ -1,3 +1,8 @@
 <section id="loading">
-    <div class="loader"></div>
+    <div class="loader">
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+    </div>
 </section>
