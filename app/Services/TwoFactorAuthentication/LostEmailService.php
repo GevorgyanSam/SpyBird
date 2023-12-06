6c05c1966120aadbd98aab3f67a0deb6f30b2e6d
GevorgyanSam <SamvelGevorgyanPC@gmail.com>
2023-12-07 00:29:27 +0400

    SpyBird Using Service For Lost Email (1.2.15)

--- app/Services/TwoFactorAuthentication/LostEmailService.php
+++ app/Services/TwoFactorAuthentication/LostEmailService.php
@@ -26,71 +26,34 @@ class LostEmailService
     public function handle($request)
     {
         $count = $this->failedLoginAttemptCheck($request);
-        if($count >= self::MAX_LOGIN_ATTEMPTS) {
+        if ($count >= self::MAX_LOGIN_ATTEMPTS) {
             return response()->json([], 429);
         }
-        if($this->missingCredentials()) {
+        if ($this->missingCredentials()) {
             return response()->json(['reload' => true], 401);
         }
         $credentials = $this->getCredentials();
         $this->vaildate($request);
         $backup_code = $this->getBackupCode($request, $credentials);
-        if(empty($backup_code) || $this->inactive($backup_code)) {
+        if (empty($backup_code) || $this->inactive($backup_code)) {
             $this->createFailedLoginAttempt($request, $credentials);
-            if(empty($backup_code)) {
+            if (empty($backup_code)) {
                 return response()->json(['errors' => ['code' => ['Wrong Code']]], 422);
             }
-            if($this->inactive($backup_code)) {
+            if ($this->inactive($backup_code)) {
                 return response()->json(['errors' => ['code' => ['This Code Was Used']]], 422);
             }
         }
         $this->destroyBackupCode($backup_code);
         $user = $this->getUser($credentials);
-
-        // ----- ----- ----- ----- ----- ----- -----
-
-        LoginInfo::where('user_id', $user->id)
-            ->where('status', 1)
-            ->update([
-                'status' => 0,
-                'updated_at' => now()
-            ]);
-        Auth::login($user);
-        $locationAction = new LocationAction();
-        $location = $locationAction($request->ip());
-        if(isset($location->message)) {
-            $location = "Not Detected";
-        } else {
-            $location = $location->country_name.', '.$location->city;
-        }
-        $loginInfo = LoginInfo::create([
-            'user_id' => Auth::user()->id,
-            'ip' => $request->ip(),
-            'user_agent' => $request->userAgent(),
-            'location' => $location,
-            'status' => 1,
-            'created_at' => now(),
-            'expires_at' => now()->addHours(self::LOGIN_EXPIRY_HOURS)
-        ]);
-        session()->forget('credentials');
-        session()->put('login-id', $loginInfo->id);
-        $cacheName = "device_".Auth::user()->id;
-        Cache::forget($cacheName);
-        $agent = new Agent();
-        $agent->setUserAgent($loginInfo->user_agent);
-        $device = $agent->device();
-        if(!$device || strtolower($device) == "webkit") {
-            $device = $agent->platform();
-        }
-        $date = Carbon::parse($loginInfo->created_at)->format('d M H:i');
-        $jobData = (object)[
-            'email' => Auth::user()->email,
-            'name' => Auth::user()->name,
-            'device' => $device,
-            'location' => $location,
-            'date' => $date
-        ];
-        NewLoginJob::dispatch($jobData);
+        $this->logoutOtherDevices($user);
+        $this->login($user);
+        $location = $this->getLocation($request);
+        $loginInfo = $this->createLoginInfo($request, $location);
+        $this->forgetCredentials($loginInfo);
+        $device = $this->getDevice($loginInfo);
+        $date = $this->getDate($loginInfo);
+        $this->sendMail($device, $location, $date);
         return response()->json(['success' => true], 200);
     }
 
@@ -194,6 +157,114 @@ class LostEmailService
     private function getUser($credentials)
     {
         return User::findOrfail($credentials->id);
+    }
+
+    // ---- ------ -- --- ------- --- -- ----- -------
+    // This Method Is For Logging Out Of Other Devices
+    // ---- ------ -- --- ------- --- -- ----- -------
+
+    private function logoutOtherDevices($user)
+    {
+        LoginInfo::where('user_id', $user->id)
+            ->where('status', 1)
+            ->update([
+                'status' => 0,
+                'updated_at' => now()
+            ]);
+    }
+
+    // ---- ------ -- --- -----
+    // This Method Is For Login
+    // ---- ------ -- --- -----
+
+    private function login($user)
+    {
+        Auth::login($user);
+    }
+
+    // ---- ------ -- --- ------- --------
+    // This Method Is For Getting Location
+    // ---- ------ -- --- ------- --------
+
+    private function getLocation($request)
+    {
+        $locationAction = new LocationAction();
+        $location = $locationAction($request->ip());
+        if (isset($location->message)) {
+            $location = "Not Detected";
+        } else {
+            $location = $location->country_name . ', ' . $location->city;
+        }
+        return $location;
+    }
+
+    // ---- ------ -- --- -------- ----- ----
+    // This Method Is For Creating Login Info
+    // ---- ------ -- --- -------- ----- ----
+
+    private function createLoginInfo($request, $location)
+    {
+        return LoginInfo::create([
+            'user_id' => Auth::user()->id,
+            'ip' => $request->ip(),
+            'user_agent' => $request->userAgent(),
+            'location' => $location,
+            'status' => 1,
+            'created_at' => now(),
+            'expires_at' => now()->addHours(self::LOGIN_EXPIRY_HOURS)
+        ]);
+    }
+
+    // ---- ------ -- --- ---------- ----------- --- -------- -----
+    // This Method Is For Forgetting Credentials And Clearing Cache
+    // ---- ------ -- --- ---------- ----------- --- -------- -----
+
+    private function forgetCredentials($loginInfo)
+    {
+        session()->forget('credentials');
+        session()->put('login-id', $loginInfo->id);
+        $cacheName = "device_" . Auth::user()->id;
+        Cache::forget($cacheName);
+    }
+
+    // ---- ------ -- --- ------- ---- ------
+    // This Method Is For Getting User Device
+    // ---- ------ -- --- ------- ---- ------
+
+    private function getDevice($loginInfo)
+    {
+        $agent = new Agent();
+        $agent->setUserAgent($loginInfo->user_agent);
+        $device = $agent->device();
+        if (!$device || strtolower($device) == "webkit") {
+            $device = $agent->platform();
+        }
+        return $device;
+    }
+
+    // ---- ------ -- --- ------- ---- -- ---- -------- ------
+    // This Method Is For Getting Date In User Friendly Format
+    // ---- ------ -- --- ------- ---- -- ---- -------- ------
+
+    private function getDate($loginInfo)
+    {
+        return Carbon::parse($loginInfo->created_at)->format('d M H:i');
+    }
+
+    // ---- ------ -- --- ------- -----
+    // This Method Is For Sending Email
+    // ---- ------ -- --- ------- -----
+
+    private function sendMail($device, $location, $date)
+    {
+        $jobData = (object) [
+            'email' => Auth::user()->email,
+            'name' => Auth::user()->name,
+            'device' => $device,
+            'location' => $location,
+            'date' => $date
+        ];
+        NewLoginJob::dispatch($jobData);
     }
 
 }
