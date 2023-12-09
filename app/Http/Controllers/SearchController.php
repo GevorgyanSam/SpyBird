5870981f3e512ecddbfcd9a4a7f8327a60e2c74f
GevorgyanSam <SamvelGevorgyanPC@gmail.com>
2023-12-09 17:03:33 +0400

    SpyBird Using Service For 'Search Contacts' In Search (1.2.289)

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,10 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
-use App\Models\User;
 use App\Services\Search\GetNearbyContactsService;
 use App\Services\Search\GetSuggestedContactsService;
+use App\Services\Search\SearchContactsService;
 
 class SearchController extends Controller
 {
@@ -33,47 +32,9 @@ class SearchController extends Controller
     // This Method Is For Searching Contacts
     // ---- ------ -- --- --------- --------
 
-    public function searchContacts(Request $request)
+    public function searchContacts(Request $request, SearchContactsService $service)
     {
-        $search = $request->input('search');
-        $users = User::with('latestLoginInfo')
-            ->select('id', 'name', 'avatar', 'activity')
-            ->where('id', '!=', auth()->user()->id)
-            ->where('status', 1)
-            ->where('invisible', 0)
-            ->where('name', 'like', "%$search%")
-            ->inRandomOrder()
-            ->limit(10)
-            ->get();
-        if (!count($users)) {
-            return response()->json(['empty' => true], 200);
-        }
-        $contacts = $users->map(function ($user) {
-            $avatar = $user->avatar;
-            if ($avatar) {
-                $avatar = asset('storage/' . $user->avatar);
-            }
-            if (!$user->activity) {
-                return [
-                    'id' => $user->id,
-                    'name' => $user->name,
-                    'avatar' => $avatar,
-                    'hidden' => true
-                ];
-            }
-            $date = $user->latestLoginInfo->updated_at;
-            if ($date) {
-                $date = Carbon::parse($date)->format('d M H:i');
-            }
-            return [
-                'id' => $user->id,
-                'name' => $user->name,
-                'avatar' => $avatar,
-                'status' => $user->latestLoginInfo->status,
-                'updated_at' => $date
-            ];
-        });
-        return response()->json(['data' => $contacts], 200);
+        return $service->handle($request);
     }
 
 }
---
+++ app/Services/Search/SearchContactsService.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Services\Search;
+
+use App\Models\User;
+use Illuminate\Support\Carbon;
+
+class SearchContactsService
+{
+
+    // --- ---- ------ --- --------- --------
+    // The Main Method For Searching Contacts
+    // --- ---- ------ --- --------- --------
+
+    public function handle($request)
+    {
+        $search = $request->input('search');
+        $users = $this->getUsers($search);
+        if ($this->missing($users)) {
+            return response()->json(['empty' => true], 200);
+        }
+        $contacts = $this->processData($users);
+        return response()->json(['data' => $contacts], 200);
+    }
+
+    // ---- ------ -- --- ------- ----- -- ----
+    // This Method Is For Getting Users By Name
+    // ---- ------ -- --- ------- ----- -- ----
+
+    private function getUsers($search)
+    {
+        return User::with('latestLoginInfo')
+            ->select('id', 'name', 'avatar', 'activity')
+            ->where('id', '!=', auth()->user()->id)
+            ->where('status', 1)
+            ->where('invisible', 0)
+            ->where('name', 'like', "%$search%")
+            ->inRandomOrder()
+            ->limit(10)
+            ->get();
+    }
+
+    // ---- ------ -- --- -------- --- ----- -----
+    // This Method Is For Checking For Empty Users
+    // ---- ------ -- --- -------- --- ----- -----
+
+    private function missing($users)
+    {
+        return !count($users) ? true : false;
+    }
+
+    // ---- ------ -- --- ---------- ---- ---- -- ----- --- --------
+    // This Method Is For Processing User Data To Array For Response
+    // ---- ------ -- --- ---------- ---- ---- -- ----- --- --------
+
+    private function processData($users)
+    {
+        return $users->map(function ($user) {
+            $avatar = $user->avatar;
+            if ($avatar) {
+                $avatar = asset('storage/' . $user->avatar);
+            }
+            if (!$user->activity) {
+                return [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'avatar' => $avatar,
+                    'hidden' => true
+                ];
+            }
+            $date = $user->latestLoginInfo->updated_at;
+            if ($date) {
+                $date = Carbon::parse($date)->format('d M H:i');
+            }
+            return [
+                'id' => $user->id,
+                'name' => $user->name,
+                'avatar' => $avatar,
+                'status' => $user->latestLoginInfo->status,
+                'updated_at' => $date
+            ];
+        });
+    }
+
+}
